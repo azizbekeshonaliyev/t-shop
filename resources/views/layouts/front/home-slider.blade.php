f05ff4e3b97efefcf6e078c36751430ce965c46e
Azizbek Eshonaliyev <1996azizbekeshonaliyev@gmail.com>
2020-12-21 17:46:35 +0500

    slide done

--- resources/views/layouts/front/home-slider.blade.php
+++ resources/views/layouts/front/home-slider.blade.php
@@ -1,15 +1,62 @@
-<section id="hero" class="hero-section top-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="hero-content">
-                    <h1 class="hero-title">{{config('app.name')}} Holiday <br> Collection</h1>
-                    <p class="hero-text">Download this amazing e-commerce web app for <strong class="text-success">FREE!</strong></p>
-                    <a class="btn btn-success" href="https://github.com/jsdecena/laracom" target="_blank" role="button">DOWNLOAD <i class="fa fa-cloud-download"></i></a>
-                    <a class="btn btn-danger" href="https://m.do.co/c/bce94237de96" target="_blank" role="button">Build on DigitalOcean <i class="fa fa-paper-plane"></i></a>
-                </div>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="lake.jpg" alt="...">
+            <div class="carousel-caption">
+                <h3  style="color: white">New York</h3>
+                <p  style="color: white">We love the Big Apple!</p>
             </div>
-            <div class="col-md-4"></div>
+        </div>
+        <div class="item">
+            <img src="tuscany428041.jpg" alt="...">
+            <div class="carousel-caption">
+                <h3 style="color: white">New York</h3>
+                <p  style="color: white"> We love the Big Apple!</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="tuscany428041.jpg" alt="...">
+            <div class="carousel-caption">
+                <h3 style="color: white">New York</h3>
+                <p  style="color: white"> We love the Big Apple!</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="tuscany428041.jpg" alt="...">
+            <div class="carousel-caption">
+                <h3 style="color: white">New York</h3>
+                <p  style="color: white"> We love the Big Apple!</p>
+            </div>
         </div>
     </div>
-</section>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<style>
+    .carousel-inner > .item > img, .carousel-inner > .item > a > img {
+        display: block;
+        height: 800px;
+        min-width: 100%;
+        width: 100%;
+        max-width: 100%;
+        line-height: 1;
+    }
+</style>
